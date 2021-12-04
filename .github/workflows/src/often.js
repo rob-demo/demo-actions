d68e0a4fdc85b031f4f8781b2072aeeba198e336
Rob Bos <raj.bos@gmail.com>
2021-12-04 20:21:06 +0100

    Moved code to separate file to find the issue

---
+++ .github/workflows/src/often.js
@@ -0,0 +1,20 @@
+module.exports = ({github, owner, repo, workflow_id}) => {
+    
+    const workflowParts = workflow_id.split('/')
+    console.log(`${JSON.stringify(workflowParts)}`)
+    const workflow = workflowParts[workflowParts.length-1]
+    
+    console.log(`Running on repo [${owner}/${repo}] with workflow_id: [${workflow}]`)
+    
+    const runs = await github.rest.actions.listWorkflowRuns({
+        owner,
+        repo,
+        workflow_id
+    });
+                
+    console.log(``)
+    console.log(`${JSON.stringify(runs)}`)
+    console.log(``)
+    console.log(`Found [${runs.length}] workflow runs`)
+
+  }
