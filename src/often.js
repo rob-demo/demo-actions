2bbde75175f8ddf365f079a407b18d9b1f0cc1d9
Rob Bos <raj.bos@gmail.com>
2021-12-04 20:33:16 +0100

    try catch the error

--- src/often.js
+++ src/often.js
@@ -6,15 +6,19 @@ module.exports = async ({github, owner, repo, workflow_id}) => {
     
     console.log(`Running on repo [${owner}/${repo}] with workflow_id: [${workflow}]`)
     
-    const runs = await github.rest.actions.listWorkflowRuns({
-        owner,
-        repo,
-        workflow_id: workflow
-    });
-                
-    console.log(``)
-    console.log(`${JSON.stringify(runs)}`)
-    console.log(``)
-    console.log(`Found [${runs.length}] workflow runs`)
+    try {
+        const runs = await github.rest.actions.listWorkflowRuns({
+            owner,
+            repo,
+            workflow_id: workflow
+        });
+                    
+        console.log(``)
+        console.log(`${JSON.stringify(runs)}`)
+        console.log(``)
+        console.log(`Found [${runs.length}] workflow runs`)
+    } catch (error) {
+        console.log(`error: ${error}`)
+    }
 
   }
