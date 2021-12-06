556914347f40c6771d8fdce5846cc91544d86948
Rob Bos <raj.bos@gmail.com>
2021-12-06 10:02:45 +0100

    try catch the error

--- src/often.js
+++ src/often.js
@@ -1,32 +1,33 @@
-module.exports = async ({github, context, owner, repo, workflow_id}) => {
+module.exports = async ({github, context, owner, repo, workflow}) => {
     
-    const workflowParts = workflow_id.split('/')
+    const workflowParts = workflow.split('/')
     console.log(`${JSON.stringify(workflowParts)}`)
-    const workflow = workflowParts[workflowParts.length-1]
+    const workflow_id = workflowParts[workflowParts.length-1]
     
-    console.log(`Running on repo [${owner}/${repo}] with workflow_id: [${workflow}]`)
+    console.log(`Running on repo [${owner}/${repo}] with workflow_id: [${workflow_id}]`)
 
     const issue = await github.rest.issues.get({
         owner: owner,
         repo: repo,
         issue_number: 6,
       })
-    console.log(`Issue: ${JSON.stringify(issue)}`)
+
+    console.log(`Issue title: ${JSON.stringify(issue.data.title)}`)
           
     try {
         console.log(`Start of the try call`)
         const creator = context.payload.sender.login
         console.log(`Creator: ${creator}`)
-        // const runs = await github.rest.actions.listWorkflowRuns({
-        //     owner,
-        //     repo,
-        //     workflow_id: workflow
-        // });
+        const runs = await github.rest.actions.listWorkflowRuns({
+            owner,
+            repo,
+            workflow_id
+        });
                     
-        // console.log(``)
-        // console.log(`${JSON.stringify(runs)}`)
-        // console.log(``)
-        // console.log(`Found [${runs.length}] workflow runs`)
+        console.log(``)
+        console.log(`${JSON.stringify(runs)}`)
+        console.log(``)
+        console.log(`Found [${runs.length}] workflow runs`)
     } catch (error) {
         console.log(`error: ${error}`)
     }
