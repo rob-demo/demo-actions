538ee3c8beadd83c68dcc53e8bc57e64678746de
Rob Bos <raj.bos@gmail.com>
2021-12-29 16:02:59 +0100

    Testing

--- src/often.js
+++ src/often.js
@@ -1,34 +1,23 @@
 module.exports = async ({github, context, owner, repo, workflow}) => {
     
-    console.log(`beginning`)
+    console.log(`beginning on workflow [${workflow}]`);
     const workflowParts = workflow.split('/')
-    console.log(`${JSON.stringify(workflowParts)}`)
     const workflow_id = workflowParts[workflowParts.length-1]
     
     console.log(`Running on repo [${owner}/${repo}] with workflow_id: [${workflow_id}]`)
-
-    const issue = await github.rest.issues.get({
-        owner: owner,
-        repo: repo,
-        issue_number: 6,
-      })
-
-    console.log(`Issue title: ${JSON.stringify(issue.data.title)}`)
           
     try {
         console.log(`Start of the try call`)
-        const creator = context.payload.sender.login
-        console.log(`Creator: ${creator}`)
 
-        const runs = await github.rest.actions.getWorkflow({
-            owner,
-            repo,
-            workflow_id
-        });
-        console.log(``)
-        console.log(`Runs: ${JSON.stringify(runs)}`)
-        console.log(``)
-        console.log(`Found [${runs.length}] workflow runs`)
+        // const runs = await github.rest.actions.getWorkflow({
+        //     owner,
+        //     repo,
+        //     workflow_id
+        // });
+        // console.log(``)
+        // console.log(`Runs: ${JSON.stringify(runs)}`)
+        // console.log(``)
+        // console.log(`Found [${runs.length}] workflow runs`)
         
         // const runs2 = await github.rest.actions.listWorkflowRunsForRepo({
         //         owner,
